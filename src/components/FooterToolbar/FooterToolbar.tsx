8d1a1163f8fcb67244e0e7111c2a625e45f8d05b
Tatiana Wansiedler <tatjana@wansiedler.com>
2024-10-27 22:31:35 +0100

    initial commit

---
+++ src/components/FooterToolbar/FooterToolbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './FooterToolbar.module.scss';
+
+const FooterToolbar: React.FC = () => {
+  return <div className={styles.toolbar}>Footer Toolbar</div>;
+};
+
+export default FooterToolbar;
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import styles from './TodoList.module.scss';
+
+const TodoList: FC = () => {
+  return <div className={styles.todoList}>Todo List </div>;
+};
+
+export default TodoList;
